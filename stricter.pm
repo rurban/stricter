d99ba56371cc52e45ece94f9d72e329bfab3a46f
Reini Urban <rurban@cpanel.net>
2015-07-10 22:25:01 +0200

    Clarifiy Artistic 2 license

--- stricter.pm
+++ stricter.pm
@@ -37,7 +37,6 @@ Reini Urban, E<lt>rurban@cpan.orgE<gt>
 Copyright (C) 2015 by cPanel Inc
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.4 or,
-at your option, any later version of Perl 5 you may have available.
+it under the terms of the Artistic License 2.0.
 
 =cut
